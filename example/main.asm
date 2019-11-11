11484f064c35c0ed07df79a70e86ed1ed77eca98
gct256 <gct256+github@gmail.com>
2019-11-11 23:17:31 +0900

    update example

--- example/main.asm
+++ example/main.asm
@@ -10,8 +10,49 @@
 
 ;;; main
 START:
-	ld	a, 0
-	ld	a, 1
+	di
+
+	; 色
+	ld	a, 0x0f
+	ld	(0xf3e9), a
+	ld	a, 0x01
+	ld	(0xf3ea), a
+	ld	a, 0x04
+	ld	(0xf3eb), a
+	call	0x0062
+
+	; SCREEN 2
+	call 0x0072
+
+	; 16x16
+	ld	a, (0xf3e0)
+	or	0x42
+	ld	b, a
+	ld	c, 1
+	call	0x0047
+
+	; SCREEN2のパターン
+	ld	bc, SC_2C - SC_2
+	ld	de, 0x0000
+	ld	hl, SC_2
+	call	0x005c
+
+	; ; SCREEN2の色
+	ld	bc, DATA_ED - SC_2C
+	ld	de, 0x2000
+	ld	hl, SC_2C
+	call	0x005c
+
+	; スプライト
+	ld	bc, SC_2 - SPR_8
+	ld	de, 0x3800
+	ld	hl, SPR_8
+	call	0x005c
+
+	ld	bc, SPR_ED - SPR_AT
+	ld	de, 0x1b00
+	ld	hl, SPR_AT
+	call	0x005c
 
 	jr	$
 
@@ -32,8 +73,12 @@ SC_2:
 	include './png/sample.msx_screen_2.png.inc'
 SC_2C:
 	include './png/sample.msx_screen_2.png.colors.inc'
+DATA_ED:
+
+SPR_AT:
+	DB	0x40, 0x00, 0, 0x0f
+	DB	0x50, 0x10, 4, 0x0e
+SPR_ED:
 
 ;;; work
 WORK:	ORG	0xc000
-	DB	0
-	DB	0
